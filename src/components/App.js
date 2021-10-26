a233d54c5ba9e445e8382a8170084b461cf07195
tech master <bryan77@twc.com>
2021-10-26 06:05:16 -0400

    creates Songlist component with a connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import SongList from './SongList';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Songs App</h1>
+      <SongList />
     </div>
   );
-}
+};
 
 export default App;
