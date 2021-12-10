da5a59526d03e2ac2017c5dbf830c2ebabdaf730
tech master <bryan77@twc.com>
2021-12-10 07:20:14 -0500

    renders songDetails component data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,11 @@ import { combineReducers } from 'redux';
 
 const songsReducer = () => {
     return {
-    songs:[        
-        { artist: "Jack Johnson", title: "Go On", duration: "4:35" },
-        { artist: "Coldplay", title: "Adventure of a Lifetime", duration: "3:43" },
+    songs:[       
+        { artist: "Coldplay", title: "Adventure of a Lifetime", duration: "3:43" }, 
+        { artist: "Jack Johnson", title: "Go On", duration: "4:35" },        
         { artist: "OneRepublic", title: "Counting Stars", duration: "4:19" },
         { artist: "U2", title: "Sometimes You Can't Make It On Your Own", duration: "4:51" }
-
     ],
     favoriteTitle: "Go On"
     }
