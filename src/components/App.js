f2c846d31829ae6b04cbf0706903dafcaff46580
tech master <bryan77@twc.com>
2022-01-01 17:48:45 -0500

    adds overall styles to app container and to favorite text

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import SongList from './SongList';
 import SongDetails from './SongDetails';
 
+const stylesContainer = {
+  marginTop: "20px",
+  backgroundColor: "#f8f5ec",
+  border: "2px solid grey",
+  boxShadow: "10px 5px 5px grey"
+}
+
 const App = () => {
   return (
-    <div className="ui container grid" style= {{marginTop: "20px"}}>
+    <div className="ui container grid" style= {stylesContainer}>
       <div className="ui row">
         <div className="column eight wide" style= {{borderRight: "2px solid rgba(34,36,38,.15)"}}>
           <SongList />
